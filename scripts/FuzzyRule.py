89f003e2796fc023faa3063a749718181f99d914
Donkeybobo <minshenhao@gmail.com>
2018-06-03 10:04:41 -0700

    tested new function - but graph output does not show properly

--- scripts/FuzzyRule.py
+++ scripts/FuzzyRule.py
@@ -1,6 +1,8 @@
 ## This file contains the FuzzyClustering class definition
 import numpy as np
 import os, sys
+import matplotlib.pyplot as plt
+
 sys.path.append(os.getcwd() + '/scripts')
 from ExtractRulesQPSO import ExtractRulesQPSO
 
@@ -16,19 +18,30 @@ class FuzzyRule:
         self.centers = np.array(centers)
         self.stds = np.array(stds)
         self.class_label = class_label
+        self.num_antecedents = len(self.centers)
         
-        print(os.getcwd())
         
     ## A function that plots the rule
     def plotRule(self):
+        
+        ## set plot parameters
+        plt.figure()
+        plt.figure(figsize=(15,7))
+        plt.rcParams['xtick.labelsize'] = 18
+        plt.rcParams['ytick.labelsize'] = 18
+        
         ## Plot every antecedent
-        plt.plot(xs, ExtractRulesQPSO._expMembership(xs, centers_c2[2][0], qpso.best_particle[10]), label = 'x1')
-        plt.plot(xs, ExtractRulesQPSO._expMembership(xs, centers_c2[2][1], qpso.best_particle[11]), label = 'x2')
-        plt.legend()
-        
-    
-                
-        
-        
-        
-        
+        for i in range(self.num_antecedents):
+            
+            # set xs, looking at mean +- 2std
+            xs = np.linspace(self.centers[i] - 2 * self.stds[i], self.centers[i] + 2 * self.stds[i], 1000)
+            
+            plt.subplot(1, self.num_antecedents, i + 1)
+            plt.plot(xs, ExtractRulesQPSO._expMembership(xs, self.centers[i], self.stds[i]), 
+                     label = 'x' + str(i + 1), linewidth = 5)
+            plt.xlabel('x' + str(i + 1), fontsize=18)       
+            
+            if (i == 0):
+                plt.ylabel('Membership', fontsize=18)
+            
+#         plt.suptitle('Rule for class ' + self.class_label, fontsize = 24)

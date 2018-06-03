0f039272135722823e8ae548f8c6d277166cfa2d
Donkeybobo <minshenhao@gmail.com>
2018-06-03 08:55:13 -0700

    import ExtractRulesQPSO class

--- scripts/FuzzyRule.py
+++ scripts/FuzzyRule.py
@@ -1,31 +1,30 @@
 ## This file contains the FuzzyClustering class definition
 import numpy as np
+import os, sys
+sys.path.append(os.getcwd() + '/scripts')
+from ExtractRulesQPSO import ExtractRulesQPSO
 
 class FuzzyRule:
     
     ## Initialize an object 
-    def __init__(self, data):
-        """This function initialize a FuzzyClustering object and set necessary parameters
-        data: 2-dimensional array
+    def __init__(self, centers, stds, class_label):
+        """This function initialize a FuzzyRule object
+        centers: a vector that has centers of all antecedents
+        stds: a vector that has stds of all antecedents
+        class_label: A string that represents the class label of this rule
         """
-        self.data = np.array(data)
-        self.cluster_centers = []
+        self.centers = np.array(centers)
+        self.stds = np.array(stds)
+        self.class_label = class_label
         
-        ### helper variable
-        self.grey_center_index = -1
+        print(os.getcwd())
         
-        # normalize data points
-        self._normalizeData()
-        
-        # initialize potentials for all data points
-        self._setInitialPotentials()
-        
-        # find the first cluster center and append it to cluster_centers
-        idx = self._getIndexWithMaxPotential()
-        self.cluster_centers.append(self.data[idx])
-        
-        # save p1star
-        self.p1star = self.potentials[idx]
+    ## A function that plots the rule
+    def plotRule(self):
+        ## Plot every antecedent
+        plt.plot(xs, ExtractRulesQPSO._expMembership(xs, centers_c2[2][0], qpso.best_particle[10]), label = 'x1')
+        plt.plot(xs, ExtractRulesQPSO._expMembership(xs, centers_c2[2][1], qpso.best_particle[11]), label = 'x2')
+        plt.legend()
         
     
                 
